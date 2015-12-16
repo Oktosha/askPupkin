247ed680e1e567da4389055a073b7d6c98c65efc
Daria Kolodzey <kolodaria@google.com>
2015-12-16 21:04:47 +0300

    Create cards for questions

--- questions/templatetags/my_tags.py
+++ questions/templatetags/my_tags.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.inclusion_tag('questions/inputfield.html')
 def input_field(field, label):
     return { 'field': field, 'label': label }
+
+@register.inclusion_tag('questions/questioncard.html')
+def question_card(question):
+    return { 'question': question }
