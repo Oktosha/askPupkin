8e083511156fc180823d792bab62d762d2b97328
Daria Kolodzey <kolodaria@google.com>
2015-12-21 17:37:49 +0300

    Add questions generation

--- questions/management/commands/filldata.py
+++ questions/management/commands/filldata.py
@@ -1,10 +1,12 @@
 from django.core.management.base import BaseCommand
-from questions.models import UserWithAvatar, Tag
+from questions.models import UserWithAvatar, Tag, Question
 from django.core.files.images import ImageFile
 import os
+from django.utils import timezone
 
 class Command(BaseCommand):
     help = 'Fills the database with dummy data'
+    lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dictum lacinia tincidunt. Quisque scelerisque nisl sed arcu tristique, vel vestibulum magna congue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor nunc id tempor gravida. Nullam non dui id felis lacinia pulvinar eu eget justo. Suspendisse luctus porttitor ultrices. Suspendisse tincidunt eleifend sapien, aliquam dapibus ligula consectetur eget. Morbi auctor justo quis arcu malesuada, eget rhoncus neque bibendum. Phasellus nec tortor ac lorem auctor condimentum. Duis venenatis felis eget ex finibus, viverra interdum sem vulputate. Fusce bibendum placerat urna nec vestibulum. Maecenas aliquet hendrerit lorem sed commodo. Nullam accumsan semper enim maximus vestibulum. Aenean vel interdum leo. Donec id orci pharetra, consequat dolor nec, tristique metus. Pellentesque molestie a sem ac varius."
 
     def create_user(self, username, avatar):
         user = UserWithAvatar.objects.create_user(username, 'dkolodzey@ya.ru', '123')
@@ -17,7 +19,16 @@ class Command(BaseCommand):
         tag.save()
 
     def create_question(self, name):
-        pass
+        user = UserWithAvatar.objects.order_by('?')[0]
+        pub_date = timezone.now()
+        title = "Could you help me with " + name + "?"
+        text = self.lorem
+        tags = Tag.objects.order_by('?')[:5]
+        question = Question(author=user, pub_date=pub_date, text=text, title=title)
+        question.save()
+        for tag in tags:
+            question.tags.add(tag)
+        question.save()
 
     def create_answer(self, name):
         pass
